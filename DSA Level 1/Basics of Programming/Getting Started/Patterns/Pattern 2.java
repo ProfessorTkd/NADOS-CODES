7735819e8d28df0f923bacf48202fa0b4a95d548
ProfessorTkd <iamtapishrawat@gmail.com>
2022-01-04 13:56:24 +0530

    Create Pattern 2.java

---
+++ DSA Level 1/Basics of Programming/Getting Started/Patterns/Pattern 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for(int i = n; i >= 1; i--){ //rows
+            for(int j = 1; j <= i; j++){ //columns
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
